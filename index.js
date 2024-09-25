370a407cf69f0d850085db70d47173a5308c3a0a
Guilherme Marques <142366424+GuilhermeMarkz@users.noreply.github.com>
2024-09-25 10:48:20 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+const tarefa = {
+    stts, 
+    conteudo: [], 
+    dataInicio,
+    dataLimite,
+    id
+};
+
+const conteudo = {
+    nome,
+    descricao,
+    id
+};
+
+const usuario = {
+    nome,
+    tarefa: [],
+    id
+};
+
+function setCookie(){
+
+};
+
+function getCookie(){
+
+};
+
+function createTarf(){
+
+};
+
+function deleteTerf(){
+
+};
+
+function listarTarf(){
+
+}
+
+function createConteudo(){
+
+}
+
+function deleteConteudo(){
+
+}
+
+function listarUsuarios(){
+
+}
+
+function listarConteudo(){
+
+}
+
+function main(){
+
+}
